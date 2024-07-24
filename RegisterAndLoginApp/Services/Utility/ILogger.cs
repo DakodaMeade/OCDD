8e0f082e6bd6b70c10502737723df68e014c5804
Dakoda Meade <dmeade1@my.gcu.edu>
2024-07-23 17:05:34 -0700

    Initial commit

---
+++ RegisterAndLoginApp/Services/Utility/ILogger.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Dakoda Meade
+ * CST-350
+ * Activity 6-2
+ * Logger interface class
+ * 
+ */
+namespace RegisterAndLoginApp.Services.Utility
+{
+    public interface ILogger
+    {
+        void Debug(string message);
+        void Error(string message);
+        void Warning(string message);
+        void Info(string message);
+    }
+}
---
+++ RegisterAndLoginApp/Services/Utility/MyLogger.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Dakoda Meade
+ * CST-350
+ * Activity 6-2
+ * Logger class
+ * 
+ */
+using NLog;
+
+namespace RegisterAndLoginApp.Services.Utility
+{
+    public class MyLogger : ILogger
+    {
+        // Singleton design pattern
+        private static MyLogger instance;
+        private static Logger logger;
+
+        public static MyLogger GetInstance() 
+        {
+            if (instance == null)
+            {
+                instance = new MyLogger();
+            }
+            return instance;
+        }
+
+        private Logger GetLogger() 
+        {
+            if (MyLogger.logger == null) 
+            {
+                MyLogger.logger = LogManager.GetLogger("LoginAppLoggerrule");
+            }
+            return MyLogger.logger;
+        }
+
+
+        public void Debug(string message)
+        {
+            GetLogger().Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            GetLogger().Error(message);
+        }
+
+        public void Info(string message)
+        {
+            GetLogger().Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            GetLogger().Warn(message);
+        }
+    }
+}
