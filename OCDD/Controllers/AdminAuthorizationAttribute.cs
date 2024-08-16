06ce2cd700025c030e919ee3a5fe34e0a2800369
Dakoda Meade <dmeade1@my.gcu.edu>
2024-08-16 16:25:55 -0700

    fdsf

--- OCDD/Controllers/AdminAuthorizationAttribute.cs
+++ OCDD/Controllers/AdminAuthorizationAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-
+//
 namespace OCDD.Controllers
 {
     public class AdminAuthorizationAttribute : Attribute, IAuthorizationFilter
