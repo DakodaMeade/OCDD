93a116cd346c9a61d48405793659b56ec1dcda76
Dakoda Meade <dmeade1@my.gcu.edu>
2024-08-17 15:28:13 -0700

    update

--- OCDD/obj/Debug/net8.0/OCDD.AssemblyInfo.cs
+++ OCDD/obj/Debug/net8.0/OCDD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OCDD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb95c47b024b21a1705a8063fbf56840b44f183")]
 [assembly: System.Reflection.AssemblyProductAttribute("OCDD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OCDD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
